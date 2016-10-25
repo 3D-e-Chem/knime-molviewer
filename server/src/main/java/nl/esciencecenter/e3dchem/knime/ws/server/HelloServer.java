e0298aaba7732b07ec726ee96966e9e234f3a71c
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2016-10-25 13:02:50 +0200

    Removed dead code

--- server/src/main/java/nl/esciencecenter/e3dchem/knime/ws/server/HelloServer.java
+++ server/src/main/java/nl/esciencecenter/e3dchem/knime/ws/server/HelloServer.java
@@ -39,63 +39,18 @@ public class HelloServer {
 		this.page = page;
 		server = new Server(port);
 		
-//		logger.warn("setupServer: HelloWorldHandler");
-//        ContextHandler hello_handler = new ContextHandler();
-//        hello_handler.setHandler(new HelloWorldHandler());
-//        hello_handler.setContextPath("/hello");
-        
-//		logger.warn("setupServer: ResourceHandler");
-//		logger.warn("x");
-//		logger.warn(HelloServer.class.getResource("/assets/"));
-//		logger.warn("x1");
-//		logger.warn(HelloServer.class.getResource("/assets"));
-//		logger.warn("x2");
-//		logger.warn(getClass().getResource("/assets"));
-//		logger.warn("x3");
-//		logger.warn(getClass().getResource("/assets/"));
-//		logger.warn("x4");
-//		logger.warn(Resource.newClassPathResource("/assets/"));
-//		logger.warn("x5");
-//		logger.warn(Resource.newClassPathResource("/assets"));
-//		try {
-//			logger.warn(Resource.newResource("/assets/"));
-//		} catch (MalformedURLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		logger.warn("x6");
-//		logger.warn(Resource.newClassPathResource("/assets/", true, true));
-//		logger.warn("x7");
-//		logger.warn(Resource.newClassPathResource("/assets", true, true));
-		
         // handle static files
-//		logger.warn("setupServer: assets");
 		String welcome_file = "index.html";
 		String class_path ="/assets/";
 		String web_path = "/assets";
         ContextHandler resource_context = staticDirHandler(welcome_file, class_path, web_path);
         ContextHandler swagger_ui_static = staticDirHandler("index.html", "/swagger-ui/", "/swagger-ui");
         
-//        logger.warn("setupServer: HelloServlet");
-//        HelloServlet hello_servlet = new HelloServlet();
-//        ServletHandler hello_servlet_handler = new ServletHandler();
-//        hello_servlet_handler.addServletWithMapping(new ServletHolder(hello_servlet), "/hello-api");
-                
-//        logger.warn("setupServer: CXFNonSpringJaxrsServlet");
-//        CXFNonSpringJaxrsServlet cxf = new CXFNonSpringJaxrsServlet();
-//        ServletHandler cxf_servlet_handler = new ServletHandler();
-//        ServletHolder cxf_holder = new ServletHolder(cxf);
-//        cxf_holder.setInitParameter("javax.ws.rs.Application", "nl.esciencecenter.e3dchem.knime.ws.server.HelloApplication");
-//        cxf_servlet_handler.addServletWithMapping(cxf_holder, "/cxf-api/*");
-//        
-
-//        logger.warn("setupServer: Jersey");
+        // setup Jersey
         ResourceConfig rc = new ResourceConfig();
         rc.register(io.swagger.jaxrs.listing.ApiListingResource.class);
         rc.register(io.swagger.jaxrs.listing.SwaggerSerializers.class);
         rc.register(SwaggerInfo.class);
-//        HelloWorldResource res = new HelloWorldResource();
-//        rc.register(res);
         sse_res = new HelloBroadcasterResource();
         rc.register(sse_res);
         ligands_res = new LigandsResource();
@@ -108,24 +63,10 @@ public class HelloServer {
         rest_handler.setContextPath("/api");
         rest_handler.addServlet(holder, "/*");
                
-//        logger.warn("setupServer: WebSocket");
-//        HelloWebSocketServlet hellows_servlet = new HelloWebSocketServlet();
-////        hellows_servlet.configure(new WebSocketServerFactory());
-//        ServletContextHandler hellows_servlet_handler = new ServletContextHandler(ServletContextHandler.SESSIONS);
-////        hellows_servlet_handler.setClassLoader(Thread.currentThread().getContextClassLoader());
-//        hellows_servlet_handler.addServlet(new ServletHolder(hellows_servlet), "/*");
-//        hellows_servlet_handler.setContextPath("/websocket");
-        
-       
-//        logger.warn("setupServer: ContextHandlerCollection");
         ContextHandlerCollection contexts = new ContextHandlerCollection();
         contexts.setHandlers(new Handler[] { 
-//        		hello_handler, 
         		resource_context, 
-//        		hello_servlet_handler, 
-//        		cxf_servlet_handler, 
         		rest_handler,
-//        		hellows_servlet_handler,
         		swagger_ui_static,
         		new DefaultHandler() 
         		});
@@ -138,7 +79,6 @@ public class HelloServer {
         resource_handler.setDirectoriesListed(true);
         resource_handler.setWelcomeFiles(new String[] {welcome_file} );
         Resource base = Resource.newClassPathResource(class_path);
-//        logger.warn(base);
         resource_handler.setBaseResource(base);
         ContextHandler resource_context = new ContextHandler();
         resource_context.setHandler(resource_handler);
@@ -149,15 +89,12 @@ public class HelloServer {
 	public void stop() throws Exception {
 		sendMessage("stop");
 		server.stop();
-//    	logger.warn("Service stopped");
 	}
 
 	public void start() throws Exception {
 		server.start();
     	int port  = ((NetworkConnector) server.getConnectors()[0]).getLocalPort();
         current_uri = new URI("http://localhost:" + port + "/assets/" + page);
-//    	logger.warn("Service started on " + current_uri.toString());
-//    	logger.warn(server.getState());
 	}
 
 	public URI getCurrentUri() {
