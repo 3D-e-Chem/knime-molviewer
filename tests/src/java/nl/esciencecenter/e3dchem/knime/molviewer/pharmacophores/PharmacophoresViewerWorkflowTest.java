79fc9f9a59d107072f04c92d6f714072bb464175
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2017-06-21 17:55:34 +0200

    Added pharmacophore viewer workflow test

---
+++ tests/src/java/nl/esciencecenter/e3dchem/knime/molviewer/pharmacophores/PharmacophoresViewerWorkflowTest.java
@@ -0,0 +1,37 @@
+package nl.esciencecenter.e3dchem.knime.molviewer.pharmacophores;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ErrorCollector;
+import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.workflow.UnsupportedWorkflowVersionException;
+import org.knime.core.util.LockFailedException;
+import org.knime.testing.core.TestrunConfiguration;
+
+import nl.esciencecenter.e3dchem.knime.testing.TestFlowRunner;
+
+public class PharmacophoresViewerWorkflowTest {
+	@Rule
+	public ErrorCollector collector = new ErrorCollector();
+	private TestFlowRunner runner;
+
+	@Before
+	public void setUp() {
+		TestrunConfiguration runConfiguration = new TestrunConfiguration();
+		runConfiguration.setTestDialogs(true);
+		runConfiguration.setTestViews(true);
+		runner = new TestFlowRunner(collector, runConfiguration);
+	}
+
+	@Test
+	public void test_simple() throws IOException, InvalidSettingsException, CanceledExecutionException,
+			UnsupportedWorkflowVersionException, LockFailedException, InterruptedException {
+		File workflowDir = new File("src/knime/molviewer-pharmacophores-test");
+		runner.runTestWorkflow(workflowDir);
+	}
+}
