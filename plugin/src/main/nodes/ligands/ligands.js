2d1ebb5b2fcfecae92afe1aa35f1f7fabdafb0e7
Stefan Verhoeven <s.verhoeven@esciencecenter.nl>
2019-09-23 11:39:23 +0200

    Added molviewer react apps as js libs + replaced ligands viewer with js node

---
+++ plugin/src/main/nodes/ligands/ligands.js
@@ -0,0 +1,85 @@
+window.molviewerLigands = function () {
+    const MolviewerLigands = {};
+    MolviewerLigands.init = function (representation, value, modules) {
+        debugger
+        // const Molviewer = modules[0];
+
+        const div = document.createElement('div');
+        div.setAttribute('id', 'viewport');
+        document.body.append(div);
+
+        const app = new window.MolViewer.LigandsViewerApp(div);
+        app.render();
+
+        const ligandsPort = representation.inObjects[0];
+        const molColumnName = value.options.ligands;
+        const molColumnIndex = ligandsPort.spec.colNames.findIndex(n => n === molColumnName);
+
+        let labelColumnIndex = undefined;
+        if ('labels' in value.options) {
+            const labelColumnName = value.options.labels;
+            labelColumnIndex = ligandsPort.spec.colNames.findIndex(n => n === labelColumnName);
+        }
+
+        const Ligands = ligandsPort.rows.map(row => {
+            return {
+                id: row.rowKey,
+                label: labelColumnIndex !== undefined ? row.data[labelColumnIndex] : row.rowKey,
+                data: row.data[molColumnIndex],
+                format: ligandsPort.spec.knimeTypes[molColumnIndex] === 'SDF' ? 'sdf' : 'mol2'
+            };
+        })
+
+        // Hookup selection events
+        // From outside to inside
+        const tableId = ligandsPort.id;
+        knimeService.subscribeToSelection(tableId, e => {
+            // TODO bi-directional selection causes infinite loop
+            // disabled recieving selections
+            if ('added' in e.changeSet) {
+                // e.changeSet.added.forEach(app.toggleVisibility);
+            }
+            if ('removed' in e.changeSet) {
+                // e.changeSet.removed.forEach(app.toggleVisibility);
+            }
+        });
+        // From inside to outside
+        app.subscribeToVisibilityToggle((toggles) => {
+            const selection = {
+                selectionMethod: "selection",
+                changeSet: {
+                    added: [],
+                    removed: []
+                }
+            };
+            toggles.forEach(toggle => {
+                if (toggle.visible) {
+                    selection.changeSet.added.push(toggle.id);
+                } else {
+                    selection.changeSet.removed.push(toggle.id);
+                }
+            });
+            knimeService.publishSelection(tableId, selection, true) 
+        })
+        app.setLigands(Ligands);
+    }
+
+    MolviewerLigands.getPNG = function () {
+        console.log('Return image');
+    }
+
+    MolviewerLigands.getComponentValue = function () {
+        return {};
+    }
+
+    MolviewerLigands.validate = function () {
+
+    }
+
+    MolviewerLigands.setValidationError = function () {
+
+    }
+
+    return MolviewerLigands;
+}();
+
--- plugin/src/main/nodes/proteins/proteins.js
+++ plugin/src/main/nodes/proteins/proteins.js
@@ -1,51 +1,81 @@
-window.molviewerProteins = function() {
+window.molviewerProteins = function () {
     const MolviewerProteins = {};
-    MolviewerProteins.init = function(representation, value, modules) {
-
-        const NGL= modules[0];
+    MolviewerProteins.init = function (representation, value, modules) {
         debugger
-        console.log('Initialized');
-        console.log([representation, value]);
+        // const Molviewer = modules[0];
 
         const div = document.createElement('div');
-        div.setAttribute('id','viewport');
+        div.setAttribute('id', 'viewport');
         document.body.append(div);
 
-        // Create NGL Stage object
-        const stage = new NGL.Stage( "viewport", {backgroundColor: "white"} );
-
-        // Handle window resizing
-        window.addEventListener( "resize", function( event ){
-            stage.handleResize();
-        }, false );
+        const app = new window.MolViewer.ProteinsViewerApp(div);
+        app.render();
 
         const proteinsPort = representation.inObjects[0];
         const proteinColumnName = value.options.pdbs;
         const proteinColumnIndex = proteinsPort.spec.colNames.findIndex(n => n === proteinColumnName);
-        // Load PDB entries from port
-        proteinsPort.rows.forEach((row, index) => {
-            const pdbData = row.data[proteinColumnIndex];
-            const stringBlob = new Blob( [ pdbData ], { type: 'text/plain'} );
-            stage.loadFile( stringBlob, { ext: "pdb", defaultRepresentation: true, name: row.rowKey } ).then((comp) => {
-                // TODO visibility based on selection
-                // comp.setVisibility(false);	
+
+        let labelColumnIndex = undefined;
+        if ('labels' in value.options) {
+            const labelColumnName = value.options.labels;
+            labelColumnIndex = proteinsPort.spec.colNames.findIndex(n => n === labelColumnName);
+        }
+
+        const proteins = proteinsPort.rows.map(row => {
+            return {
+                id: row.rowKey,
+                label: labelColumnIndex !== undefined ? row.data[labelColumnIndex] : row.rowKey,
+                data: row.data[proteinColumnIndex],
+                format: proteinsPort.spec.knimeTypes[proteinColumnIndex] === 'PDB' ? 'pdb' : 'mol2'
+            };
+        })
+        app.setProteins(proteins);
+
+        // Hookup selection events
+        // From outside to inside
+        const tableId = proteinsPort.id;
+        knimeService.subscribeToSelection(tableId, e => {
+            if ('added' in e.changeSet) {
+                e.changeSet.added.forEach(app.toggleVisibility);
+            }
+            if ('removed' in e.changeSet) {
+                e.changeSet.removed.forEach(app.toggleVisibility);
+            }
+        });
+        // From inside to outside
+        app.subscribeToVisibilityToggle((toggles) => {
+            const selection = {
+                selectionMethod: "selection",
+                changeSet: {
+                    added: [],
+                    removed: []
+                }
+            };
+            toggles.forEach(toggle => {
+                if (toggle.visible) {
+                    selection.changeSet.added.push(toggle.id);
+                } else {
+                    selection.changeSet.removed.push(toggle.id);
+                }
             });
-        });       
+            // TODO bi-directional selection causes infinite loop
+            // knimeService.publishSelection(tableId, selection, true) 
+        })
     }
 
     MolviewerProteins.getPNG = function () {
         console.log('Return image');
     }
 
-    MolviewerProteins.getComponentValue = function() {
+    MolviewerProteins.getComponentValue = function () {
         return {};
     }
 
-    MolviewerProteins.validate = function() {
+    MolviewerProteins.validate = function () {
 
     }
 
-    MolviewerProteins.setValidationError = function() {
+    MolviewerProteins.setValidationError = function () {
 
     }
 
